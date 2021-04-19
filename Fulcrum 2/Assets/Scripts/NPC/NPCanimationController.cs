285d2814d5a7defe84585e9edb3204765fba3dc4
noah reynolds <noahreynolds7288@gmail.com>
2021-04-19 17:18:26 -0500

    added Bug prefab

--- Fulcrum 2/Assets/Scripts/NPC/NPCanimationController.cs
+++ Fulcrum 2/Assets/Scripts/NPC/NPCanimationController.cs
@@ -6,7 +6,6 @@ public class NPCanimationController : MonoBehaviour
     public Animator animator;
     NavMeshAgent agent;
     private Rigidbody myRigidBody;
-    public Transform player;
 
     void start()
     {
@@ -16,17 +15,9 @@ public class NPCanimationController : MonoBehaviour
 
     void update()
     {
-        Vector3 direction = player.position - this.transform.position;
-        direction.y = 0;
+        
 
-            if(direction.magnitude > 1)
-        {
-            animator.SetBool("isMoving", true);
-        }
-        else
-        {
-            animator.SetBool("isMoving", false);
-        }
+       
 
         
     }
